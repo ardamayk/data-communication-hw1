4fda28f53fc482fe08275b0f3dccad9d84564b57
Arda Mike <ardamike21@hotmail.com>
2025-04-18 01:03:53 +0300

    Ignore build folder

--- viba/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/deploy_viba_40f4ac0cbd.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/mert/Documents/viba/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/viba-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/mert/Documents/viba/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/viba.exe
-    GENERATE_QT_CONF
-)
